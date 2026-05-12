b2ec9ec1e22afa9e9493ea73c527ccb012cb2e93
diegoipasg23-alt <diego.ipasg23@gmail.com>
2026-05-12 10:12:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rpps-v1';
-const FILES=['/index.html','/manifest.json'];
+const CACHE='rpps-v2';
+const FILES=['/RPPS-GESTOR/','/RPPS-GESTOR/index.html','/RPPS-GESTOR/manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
